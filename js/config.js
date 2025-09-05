890fd94220b28cb76ca77c337dd14c60ec94b510
ShivamKJ1 <shivam.work166@gmail.com>
2025-09-05 19:33:59 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 window._config = {
-    cognito: {
-        userPoolId: 'ap-south-1_dlnyrhR9n', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3gttv0i7nnuucn23n82m3hasuj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-south-1' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: 'ap-south-1_dlnyrhR9n',
+    userPoolClientId: '3gttv0i7nnuucn23n82m3hasuj',
+    clientSecret: 'aog8mmj873rg2b8jnmgqp80ih0qmt32psl715qpj33p1bc9avue',
+    region: 'ap-south-1'
+  },
+  api: { invokeUrl: '' }
 };
+
 
