005d88bbd2a6b8e9a20dc3ee868d86d181f2c041
ShivamKJ1 <shivam.work166@gmail.com>
2025-09-05 19:44:59 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
   cognito: {
     userPoolId: 'ap-south-1_dlnyrhR9n',
-    userPoolClientId: '3gttv0i7nnuucn23n82m3hasuj',
-    clientSecret: 'aog8mmj873rg2b8jnmgqp80ih0qmt32psl715qpj33p1bc9avue',
+    userPoolClientId: 'mogupa0oggi03fdlf8peljtne',
+    // clientSecret: 'aog8mmj873rg2b8jnmgqp80ih0qmt32psl715qpj33p1bc9avue',
     region: 'ap-south-1'
   },
   api: { invokeUrl: '' }
 };
+
 
 
